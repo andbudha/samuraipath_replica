e7c5cb20a58688863961dfcec3513c6062dd4dcc
andbudha <andreibartov@gmail.com>
2022-10-31 21:45:43 +0100

    app updated/css modules created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from "./Header.module.css";
 
 const Header = () => {
     return (
         <>
-            <header className={'header'}>
+            <header className={classes.header}>
                 <img src="https://png2.cleanpng.com/sh/8f7ab2980f5cfef5754f9db18e51be9a/L0KzQYm3VsI0N6d1ipH0aYP2gLBuTfdmbZwyhNHwbz32hLFqi71xcJD5h9l7YYDriX68gsJnOGE2UKJsYkW6Q3A6UccxOWQATaMAMkm7QIeBV8IxPWM2RuJ3Zx==/kisspng-geek-logo-stock-photography-5b2f00180cb573.3170139515298068720521.png" alt="logo"/>
             </header>
         </>
