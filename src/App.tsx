233d9a2b35f4076cb4641c8d6683c668a0b4ad19
andbudha <andreibartov@gmail.com>
2022-11-02 20:54:54 +0100

    component Dialogues added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogues from "./components/Dialogues/Dialogues";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Header/>
             <Navbar/>
             <Profile/>
+            <Dialogues/>
         </div>
     );
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -7,7 +7,7 @@ const Profile = () => {
         <>
             <div className={classes.content}>
                 <div>
-                    <img src="https://pixabay.com/get/g186956ebf29b496956d0d89ea43b71dd0cf37bb555340faa3aba68ba7674aa529b0d52513174cd42d85e4c7294c62d5d1514ee5369094d44a2c4ae21127eb9464139eb763bbe1348bd197aae3bd8b845_1920.jpg" alt="desert image"/>
+                    <img src="https://wallpapercave.com/wp/wp2822939.jpg" alt="city-image"/>
                 </div>
                 <div>
                     avatar+description
