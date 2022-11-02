46fb92336585cb3feea41afed5986d9d1b3ae201
andbudha <andreibartov@gmail.com>
2022-11-02 21:58:51 +0100

    routing implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogues from "./components/Dialogues/Dialogues";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
-        <div className={'app-wrapper'}>
-            <Header/>
-            <Navbar/>
-            <div className={'app-wrapper-content'}>
-                { /*<Dialogues/>*/}
-                <Profile/>
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <Header/>
+                <Navbar/>
+                <div className={'app-wrapper-content'}>
+                    <Route path={'/dialogues'} component={Dialogues}/>
+                    <Route path={'/profile'} component={Profile}/>
+                </div>
             </div>
-
-        </div>
+        </BrowserRouter>
     );
 }
 
