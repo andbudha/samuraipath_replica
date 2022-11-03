958f023043741714353d2d96687464465730af33
andbudha <andreibartov@gmail.com>
2022-11-03 21:47:26 +0100

    component <Dialogues/>updated

--- src/components/Dialogues/Dialogues.tsx
+++ src/components/Dialogues/Dialogues.tsx
@@ -3,8 +3,26 @@ import classes from "./Dialogues.module.css";
 
 const Dialogues = () => {
     return (
-        <div>
-            Dialogues
+        <div className={classes.dialogues}>
+            <div className={classes.dialogue_items}>
+                <div className={`${classes.dialogue} ${classes.active}`}>
+                    Andrei
+                </div>
+                <div className={classes.dialogue}>
+                    Delia
+                </div>
+                <div className={classes.dialogue}>
+                    Natalia
+                </div>
+                <div className={classes.dialogue}>
+                    Dorian
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi there...</div>
+                <div className={classes.message}>How is your it-kamasutra?</div>
+                <div className={classes.message}>What's up?</div>
+            </div>
         </div>
     );
 };
