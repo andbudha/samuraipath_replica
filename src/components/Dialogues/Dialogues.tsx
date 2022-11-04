ee498feef439b6e17282673573849fc871cd704f
andbudha <andreibartov@gmail.com>
2022-11-04 21:53:31 +0100

    component <Dialogues/>updated

--- src/components/Dialogues/Dialogues.tsx
+++ src/components/Dialogues/Dialogues.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import classes from "./Dialogues.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogues = () => {
     return (
         <div className={classes.dialogues}>
             <div className={classes.dialogue_items}>
                 <div className={`${classes.dialogue} ${classes.active}`}>
-                    Andrei
+                    <NavLink to='/dialogues/1'>Andrei</NavLink>
                 </div>
                 <div className={classes.dialogue}>
-                    Delia
+                    <NavLink to='/dialogues/2'>Delia</NavLink>
                 </div>
                 <div className={classes.dialogue}>
-                    Natalia
+                    <NavLink to='/dialogues/3'>Natalia</NavLink>
                 </div>
                 <div className={classes.dialogue}>
-                    Dorian
+                    <NavLink to='/dialogues/4'>Dorian</NavLink>
                 </div>
             </div>
             <div className={classes.messages}>
