13b407f4727f5a09ea4b4f6ce79df7ecbc7149b4
andbudha <andreibartov@gmail.com>
2022-11-06 14:35:47 +0100

    component MyPosts updated

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,11 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postData = [
+        {message: 'Hi, how are you doing?', likes: 15 },
+        {message: 'It is my very first post!', likes: 25 }
+    ]
     return (
         <div className={classes.postsBlock}>
             <h3>My Posts</h3>
@@ -15,8 +20,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message={'Hi, how are you doing?'} likes={15}/>
-                <Post message={'It is my very first post!'} likes={25}/>
+                <Post message={postData[0].message} likes={postData[0].likes}/>
+                <Post message={postData[1].message} likes={postData[1].likes}/>
             </div>
         </div>
     );
