d4cbd47a89fa4fa3ab9b071a966998280c3a67a9
andbudha <andreibartov@gmail.com>
2022-11-17 23:13:45 +0100

    MyPosts updated/onClick ev.-handler applied

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -12,16 +12,23 @@ const MyPosts = (props: MyPostsPropsType) => {
     let postElements = props.posts
         .map(p=><Post message={p.message} likes={p.likes}/>);
 
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+    const addPost = () => {
+        let text = newPostElement.current?.value;
+      alert(text);
+    }
+
 
     return (
         <div className={classes.postsBlock}>
             <h3>My Posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add Post</button>
+                    <button onClick={addPost}>Add Post</button>
                 </div>
             </div>
             <div className={classes.posts}>
